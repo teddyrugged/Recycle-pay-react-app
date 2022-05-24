a3205bc175f600888f0dbc0d366355cb0a1aed4f
kristofajosh <christopherjoshua18@live.com>
2022-05-24 11:49:31 +0100

    chore: setup sample test

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -4,6 +4,6 @@ import PropTypes from 'prop-types';
 export const Button = ({ text, children }) => <button type="button">{text || children}</button>;
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   text: PropTypes.string,
 };
--- src/components/elements/Text/index.jsx
+++ src/components/elements/Text/index.jsx
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 
 export const Text = ({ text, children }) => <p>{text || children}</p>;
 
-export const Heading = ({ text, children, level, ...props }) => React.createElement(`h${level || 1}`, { ...props }, text || children);
+export const Heading = ({ text, children, level, ...props }) =>
+  React.createElement(`h${level || 1}`, { ...props }, text || children);
 
 Text.Heading = Heading;
 
 Text.propTypes = {
   text: PropTypes.string,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 };
 
 Heading.propTypes = {
   text: PropTypes.string,
   level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 };
