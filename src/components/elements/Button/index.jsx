c2d376a34b0ba83e3efa988717241845cf439e1e
kristofajosh <christopherjoshua18@live.com>
2022-05-30 10:34:08 +0100

    feat: create button

--- src/components/elements/Button/index.jsx
+++ src/components/elements/Button/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css';
+import clsx from 'clsx';
 
-export const Button = ({ text, children }) => <button type="button">{text || children}</button>;
+export const Button = ({ text, btnType, stretch, round, children, ...props }) => (
+  <button
+    className={clsx(`button-base ${btnType || 'primary'}`, [
+      { stretch, round, 'round-secondary': round && btnType === 'secondary' },
+    ])}
+    type="button"
+    {...props}
+  >
+    {text || children}
+  </button>
+);
 
 Button.propTypes = {
   children: PropTypes.node,
   text: PropTypes.string,
+  btnType: PropTypes.oneOf(['primary', 'secondary']),
+  stretch: PropTypes.bool,
+  round: PropTypes.bool,
 };
