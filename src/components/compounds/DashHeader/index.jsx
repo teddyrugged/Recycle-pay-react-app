e0458551a6f7d257746300f1ae2a16f015fbd1c0
kramstyles <mark.eke@decagon.dev>
2022-05-31 15:19:36 +0100

    added input field to header

--- src/components/compounds/DashHeader/index.jsx
+++ src/components/compounds/DashHeader/index.jsx
@@ -1,11 +1,14 @@
 import './style.css';
-import { Text } from '../../elements';
+import { Text, SearchBar } from '../../elements';
 
 export const DashHeader = () => (
   <div className="dash-navbar">
-    <div>Hello</div>
+    <div>
+      <SearchBar />
+    </div>
     <div className="name-holder">
-      <Text text="Hello Dash" />
+      <Text text="David Michael" />
+      <span className="h-space" />
       <span className="name-icon">Y</span>
     </div>
   </div>
---
+++ src/components/elements/SearchBar/index.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const SearchBar = () => (
+  <div className="search-holder">
+    <i className="fa fa-search" />
+    <span className="h-space" />
+    <input />
+  </div>
+);
