8f6df9abf5835dd183f08e5d96b3f78ddb00a35b
Akorede olawole <akoredeolawole@gmail.com>
2022-06-05 20:54:07 +0100

    added routes to forgot password pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import { AnotherPage, SignUpPage, SignInPage } from 'pages';
+import { AnotherPage, SignUpPage, SignInPage, PasswordResetOne, PasswordResetThree, PasswordResetFive } from 'pages';
 
 const App = () => (
   <div className="App">
     <Routes>
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/signin" element={<SignInPage />} />
+      <Route path="/forget-one" element={<PasswordResetOne />} />
+      <Route path="/forget-three" element={<PasswordResetThree />} />
+      <Route path="/forget-five" element={<PasswordResetFive />} />
       <Route path="/page-2" element={<AnotherPage />} />
     </Routes>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { SignUpPage } from 'pages/Auth';
 export { SignInPage } from 'pages/Auth';
 export { AnotherPage } from 'pages/AnotherPage';
+export { PasswordResetOne } from 'pages/Auth';
+export { PasswordResetThree } from 'pages/Auth';
+export { PasswordResetFive } from 'pages/Auth';
