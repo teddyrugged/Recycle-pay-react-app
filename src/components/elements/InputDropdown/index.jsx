47dd40e585e5ad8abccaf5defe8b2b91bcffaad2
kramstyles <mark.eke@decagon.dev>
2022-06-07 22:22:28 +0100

    worked on normal input dropdown

--- src/components/elements/InputDropdown/index.jsx
+++ src/components/elements/InputDropdown/index.jsx
@@ -9,16 +9,22 @@ const basePropTypes = {
   dropdown: PropTypes.bool,
 };
 
-export const InputDropdown = ({ title, date, dropdown }) => (
-  <div className="InputDropdown-body">
-    <Text text={title} />
-    <div className="InputDropdown-input">
-      {date ? <i className="fa-regular fa-calendar" /> : ''}
-      {dropdown ? <i className="fa fa-chevron-down" /> : ''}
-      <input type="text" disabled />
+export const InputDropdown = ({ title, date, dropdown }) => {
+  // Code to make input dropdown accept inputs
+  let disabled = 'disabled';
+  if (!date && !dropdown) disabled = '';
+
+  return (
+    <div className="InputDropdown-body">
+      <Text text={title} />
+      <div className="InputDropdown-input">
+        {date ? <i className="fa-regular fa-calendar" /> : ''}
+        {dropdown ? <i className="fa fa-chevron-down" /> : ''}
+        <input type="text" disabled={disabled} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 InputDropdown.propTypes = {
   ...basePropTypes,
