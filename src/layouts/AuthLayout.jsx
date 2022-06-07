fa4f7671edd78ffe548c0147b60b0b9f7f3fcdc5
kristofajosh <christopherjoshua18@live.com>
2022-06-07 10:36:27 +0100

    fix: html semantics and prop usage

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 import { Text } from 'components';
 import clsx from 'clsx';
 
-export const AuthLayout = ({ children, className, ...props }) => (
-  <header className={clsx('nav-container', className)} {...props}>
-    <div>
+export const AuthLayout = ({ children, headerClassName, bodyClassName }) => (
+  <>
+    <header className={clsx('nav-container', headerClassName)}>
       <Text size={24}>Recycle Pay</Text>
-    </div>
-    <div className="form-container">{children}</div>
-  </header>
+    </header>
+    <div className={clsx('form-container', bodyClassName)}>{children}</div>
+  </>
 );
 
 AuthLayout.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string,
+  headerClassName: PropTypes.string,
+  bodyClassName: PropTypes.string,
 };
