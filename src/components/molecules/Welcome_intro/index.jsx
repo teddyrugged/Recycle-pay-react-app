30454e0b320d2010d7b3ccf50b21f0bb09fd2708
Decagon <decagon@Decagons-MacBook-Air-7.local>
2022-06-08 14:21:37 +0100

    welcome_intro modified

--- src/components/molecules/Welcome_intro/index.jsx
+++ src/components/molecules/Welcome_intro/index.jsx
@@ -1,8 +1,15 @@
 import { Button, Text } from 'components';
 
+import './style.css';
+
 export const Welcome = () => (
-  <div>
-    <Button>Hello Prettier</Button>
-    <Text text="I am a text" />
+  <div className="intro_body">
+    <Text size={16} weight={450}>
+      <span>Welcome Ade! </span>
+      We’re excited to have you contribute to your environmental health today.
+    </Text>
+    <Button round btnType="primary">
+      <Text size={14}> Request A PickUp </Text>
+    </Button>
   </div>
 );
