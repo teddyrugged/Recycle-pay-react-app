a04c439db36ffb5fe3bdc4bb37545725b6120fb0
kramstyles <mark.eke@decagon.dev>
2022-06-16 10:29:27 +0100

    fixed design for reset-one

--- src/pages/Auth/PasswordResetOne/index.jsx
+++ src/pages/Auth/PasswordResetOne/index.jsx
@@ -1,17 +1,24 @@
+import { Button, Text, Input } from 'components';
+import { Link } from 'react-router-dom';
 import React from 'react';
-import { AuthLayout } from 'layouts';
-import { Button, Text } from 'components';
 
 export const PasswordResetOne = () => (
-  <AuthLayout>
+  <div className="wrapper-container">
+    <Text.Heading text="Reset Password" size={24} weight={500} level={1} />
     <div className="wrapper--email">
       <div className="wrapper--label--email">
         <Text text="Enter your Email Address" />
       </div>
       <div className="wrapper--input--email">
-        <div>pass</div>
+        <Input.FullRound name="email" type="email" placeholder="info@recyclepay.com" />
       </div>
     </div>
     <Button stretch btnType="primary" text="Send Recovery Link" />
-  </AuthLayout>
+    <div className="signin-main-footer-text">
+      <Text.Heading text="Don't have an account?" color="grey" size={14} weight={500} level={4} />
+      <Link to="/signup">
+        <Text.Heading text="Sign Up" color="green" size={14} weight={500} level={4} />
+      </Link>
+    </div>
+  </div>
 );
