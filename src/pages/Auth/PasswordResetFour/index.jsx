b4705fe47bb3fcb0857afb148336c46656a9a8b8
kramstyles <mark.eke@decagon.dev>
2022-06-16 11:21:07 +0100

    fixed display for 2-4

--- src/pages/Auth/PasswordResetFour/index.jsx
+++ src/pages/Auth/PasswordResetFour/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { AuthLayout } from 'layouts';
 import { Text, Button } from 'components';
 
 export const PasswordResetFour = () => (
-  <AuthLayout>
-    <Text size={20}>Reset password</Text>
-    <Text size={24}>
+  <div className="wrapper-container">
+    <Text.Heading text="Reset Password" size={24} weight={500} level={1} />
+    <Text size={18} className="wrapper--text">
       We’ve sent an email to you.
       <br /> Kindly activate your account from the link in the email sent.
     </Text>
     <Button stretch btnType="primary" text="Resend Recovery Link" />
-  </AuthLayout>
+  </div>
 );
